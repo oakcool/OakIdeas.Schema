bbad54719a76bf349266e900c77a13bec9196eb3
mdecarvalho <oakcool@yahoo.com>
2015-04-28 17:26:30 -0700

    for some reason I had 2 untracked objects

---
+++ OakIdeas.Schema.Microdata/Products/IndividualProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OakIdeas.Schema.Microdata.Products
+{
+    /// <summary>
+    /// A single, identifiable product instance (e.g. a laptop with a particular serial number).
+    /// </summary>
+    /// <see cref="http://www.schema.org/IndividualProduct"/>
+    public class IndividualProduct : Product
+    {
+        /// <summary>
+        /// The serial number or any alphanumeric identifier of a particular product. When attached to an offer, it is a shortcut for the serial number of the product included in the offer.
+        /// </summary>
+        public string SerialNumber { get; set; }
+    }
+}
