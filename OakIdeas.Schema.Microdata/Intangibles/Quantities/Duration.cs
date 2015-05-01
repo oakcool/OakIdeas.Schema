81c75b92bf1bab615676862d300460b4a4ee4e54
mdecarvalho <oakcool@yahoo.com>
2015-05-01 13:00:35 -0700

    Got all the level 1 Organizations

---
+++ OakIdeas.Schema.Microdata/Intangibles/Quantities/Duration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OakIdeas.Schema.Microdata.Intangibles.Quantities
+{
+    /// <summary>
+    /// Quantity: Duration (use ISO 8601 duration format).
+    /// </summary>
+    public class Duration: Quantity
+    {
+    }
+}
---
+++ OakIdeas.Schema.Microdata/Organizations/Corporation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OakIdeas.Schema.Microdata.Organizations
+{
+    /// <summary>
+    /// Organization: A business corporation.
+    /// </summary>
+    /// <see cref="http://www.schema.org/Corporation"/>
+    public class Corporation: Organization
+    {
+        /// <summary>
+        /// The exchange traded instrument associated with a Corporation object. 
+        /// The tickerSymbol is expressed as an exchange and an instrument name separated by a space character. 
+        /// For the exchange component of the tickerSymbol attribute, we reccommend using the controlled vocaulary of Market Identifier Codes (MIC) specified in ISO15022.
+        /// </summary>
+        public string TickerSymbol { get; set; }
+    }
+}
