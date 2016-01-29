bb22f152346fff6dc6937390335ac33e2b85b39e
Mateus <oakcool@yahoo.com>
2016-01-28 22:00:46 -0800

    A few classes that were not being tracked, plus a couple goods

---
+++ OakIdeas.Schema.Core/Products/SomeProducts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OakIdeas.Schema.Core.Intangibles.StructuredValues;
+
+namespace OakIdeas.Schema.Core.Products
+{
+    /// <summary>
+    /// A placeholder for multiple similar products of the same kind.
+    /// </summary>
+    /// <see cref="http://www.schema.org/SomeProducts"/>
+    public class SomeProducts : Product
+    {
+        /// <summary>
+        /// The current approximate inventory level for the item or items.
+        /// </summary>
+        public QuantitativeValue InventoryLevel { get; set; }
+    }
+}
---
+++ OakIdeas.Schema.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OakIdeas.Schema.Core")]
+[assembly: AssemblyDescription("Schema.org .NET implementation")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OakIdeas")]
+[assembly: AssemblyProduct("OakIdeas.Schema.Core")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("OakIdeas")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
